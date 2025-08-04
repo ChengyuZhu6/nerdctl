e2cd0aadd6c90f31e3c0c80d34d06a838ba7a60b
ChengyuZhu6 <hudson@cyzhu.com>
2025-08-04 15:55:51 +0800

    manifest: support nerdctl manifest annotate command
    Signed-off-by: ChengyuZhu6 <hudson@cyzhu.com>

--- pkg/manifestutil/manifestutils.go
+++ pkg/manifestutil/manifestutils.go
@@ -34,6 +34,49 @@ import (
 	"github.com/containerd/nerdctl/v2/pkg/referenceutil"
 )
 
+type osArch struct {
+	os   string
+	arch string
+}
+
+// List of valid os/arch values
+// From: https://github.com/docker/cli/blob/v20.10.0/cli/command/manifest/util.go#L22-L51
+var validOSArches = map[osArch]bool{
+	{os: "darwin", arch: "386"}:      true,
+	{os: "darwin", arch: "amd64"}:    true,
+	{os: "darwin", arch: "arm"}:      true,
+	{os: "darwin", arch: "arm64"}:    true,
+	{os: "dragonfly", arch: "amd64"}: true,
+	{os: "freebsd", arch: "386"}:     true,
+	{os: "freebsd", arch: "amd64"}:   true,
+	{os: "freebsd", arch: "arm"}:     true,
+	{os: "linux", arch: "386"}:       true,
+	{os: "linux", arch: "amd64"}:     true,
+	{os: "linux", arch: "arm"}:       true,
+	{os: "linux", arch: "arm64"}:     true,
+	{os: "linux", arch: "ppc64le"}:   true,
+	{os: "linux", arch: "mips64"}:    true,
+	{os: "linux", arch: "mips64le"}:  true,
+	{os: "linux", arch: "riscv64"}:   true,
+	{os: "linux", arch: "s390x"}:     true,
+	{os: "netbsd", arch: "386"}:      true,
+	{os: "netbsd", arch: "amd64"}:    true,
+	{os: "netbsd", arch: "arm"}:      true,
+	{os: "openbsd", arch: "386"}:     true,
+	{os: "openbsd", arch: "amd64"}:   true,
+	{os: "openbsd", arch: "arm"}:     true,
+	{os: "plan9", arch: "386"}:       true,
+	{os: "plan9", arch: "amd64"}:     true,
+	{os: "solaris", arch: "amd64"}:   true,
+	{os: "windows", arch: "386"}:     true,
+	{os: "windows", arch: "amd64"}:   true,
+}
+
+func IsValidOSArch(os string, arch string) bool {
+	_, ok := validOSArches[osArch{os, arch}]
+	return ok
+}
+
 // manifestParser defines a function type for parsing manifest data
 type manifestParser func([]byte) (interface{}, error)
 
